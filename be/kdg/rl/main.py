e1e4e2a4f6e9e95d94d939692cd253f007a4c221
Andreas Van Loon <andreas.vanloon@student.kdg.be>
2023-06-16 17:51:13 +0200

    additions

--- be/kdg/rl/main.py
+++ be/kdg/rl/main.py
@@ -18,7 +18,30 @@ plt.rcParams.update({'font.size': 17})
 
 if __name__ == '__main__':
 
-    # matplotlib.use("TkAgg")  # activate tkinter
+    # https://towardsdatascience.com/the-values-of-actions-in-reinforcement-learning-using-q-learning-cb4b03be5c81
+    # Algemeen Q learning algo :
+
+    # Hyperparameters
+    # ---------------
+    # Bepaal de stepsize alfa tussen 0 en 1
+    # Bepaal epsilon tussen 0 en 1 voor eploitation/exploration tradeoff
+    # Bepaal de discount factor gamma voor future rewards en Gt bepaling
+    # Initialiseer de Q tabel waar alle waardes random zijn behalve voor de terminal points zoals cadeau en gat
+    # Bepaal het aantal episodes N
+    #
+    # Itereer van 1 tot N (aantal episodes)
+    # -------------------------------------
+    # neem een start state s
+    # itereer tot de agent een terminal state bereikt
+    # in een gegeven state s, kies een actie a door gebruik te maken van epsilon greedy
+    # neem deze actie a en observeer (percept) wat de overgangsstate s' en zijn reward r zijn
+    # update de Q tabel Q(S,A) + alfa*(r + gamma*maxQ(s',a) - Q(S,A)
+    # Q(S,A) state waar onze agent zich in bevindt
+    # maxQ(s',a) deel is de maximaal beschikbare Q-waarde in de staat waarin onze agent terechtkomt, over alle acties heen
+    # en r is reward voor naar s' te gaan
+    # set state s naar s'
+
+
     experiment = parameters.current_experiment  # zie parameters.py
     parameters.init()  # initialize local environment
 
